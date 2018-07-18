2ee1ea59d381a6b6cb2ddae6f2e26680d83125d3
young <lightwish@126.com>
2018-07-18 22:35:13 +0800

    day one

--- db.h
+++ db.h
@@ -1,5 +1,5 @@
-#ifndef __db_h
-#define __db_h
+#ifndef _db_h
+#define _db_h
 
 #define DB_FILE "/usr/local/.devpkg/db"
 #define DB_DIR "/usr/local/.devpkg"
--- dbg.h
+++ dbg.h
@@ -7,14 +7,14 @@
 #ifdef NODEBUG
 #define debug(M,...)
 #else
-#define debug(m,...) fprintf("Debug:%s,%d" M "\n",__FILE,__LINE__,##VA__ARGS__)
+#define debug(m,...) fprintf("Debug:%s,%d" M "\n",__FILE,__LINE__,##__VA_ARGS__)
 #define clean_errno() (errno==0?"None":strerror(errno))
 #define log_err(M,...) fprintf("[ERROR] (%s:%d: errno:%s)" M "\n",__FILE__,__LINE__,clean_errno())
-#define log_info(M,...) fprintf("[INFO] (%s:%d:)" M "\n",__FILE__,__LINE__,##VA__ARGS__)
-#define log_warn(M,...) fprintf("[WARN] (%s:%d: errno:%s)" M "\n",__FILE__,__LINE__,clean_errno(),##VA__ARGS__)
-#define check(A,M,...) if(!(A)) {log_err(M, ##VA__ARGS__);errno=0;goto error;}
-#define sentinel(M, ...)  {log_err(M, ##VA__ARGS__);errno=0;goto error;}
+#define log_info(M,...) fprintf("[INFO] (%s:%d:)" M "\n",__FILE__,__LINE__,##__VA_ARGS__)
+#define log_warn(M,...) fprintf("[WARN] (%s:%d: errno:%s)" M "\n",__FILE__,__LINE__,clean_errno(),##__VA_ARGS__)
+#define check(A,M,...) if(!(A)) {log_err(M, ##__VA_ARGS__);errno=0;goto error;}
+#define sentinel(M, ...)  {log_err(M, ##__VA_ARGS__);errno=0;goto error;}
 #define check_mem(A) check(A, "out of memory")
-#define check_debug(A,M,...) if(!(A)) {debug(M,##VA__ARGS__;errno=0;goto error;)}
+#define check_debug(A,M,...) if(!(A)) {debug(M,##__VA_ARGS__;errno=0;goto error;)}
 #endif
 #endif
